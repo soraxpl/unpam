26d8e5c6d36aba46a9ca85010efd5073619010a4
Sora <125755859+soraxpl@users.noreply.github.com>
2024-07-09 22:18:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -39,4 +39,5 @@ def check_status():
     return jsonify({"url": url, "status": status})
 
 if __name__ == '__main__':
-    app.run(port=80, host='0.0.0.0')
+    # app.run(port=80, host='0.0.0.0')
+    app.run()
